893dc72b22cc448da58aaa7cde4bf764b3765941
Taylor Masiello <masiellotay@gmail.com>
2022-04-25 15:35:21 -0700

    Share data; add new order config dialog; sizing, orderState, cancel, submit; all implemented and functional;

--- Program.cs
+++ Program.cs
@@ -5,9 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-
 builder.Services.AddHttpClient();
 builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
+
+builder.Services.AddScoped<OrderState>();
 
 var app = builder.Build();
 
