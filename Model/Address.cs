efd27143ece93f6c2ab26001cfd5824bc8e02e4c
Taylor Masiello <masiellotay@gmail.com>
2022-05-09 05:28:34 -0700

    Added server-side validation to orderDetail page Address form

--- Model/Address.cs
+++ Model/Address.cs
@@ -1,20 +1,27 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace BlazingPizza
 {
-    public class Address
-    {
-        public int Id { get; set; }
+public class Address
+{
+    public int Id { get; set; }
 
-        public string Name { get; set; }
+    [Required, MinLength(3, ErrorMessage = "Please use a Name bigger than 3 letters."), MaxLength(100, ErrorMessage = "Please use a Name less than 100 letters.")]
+    public string Name { get; set; }
 
-        public string Line1 { get; set; }
+    [Required, MinLength(5, ErrorMessage = "Please use an Address bigger than 5 letters."), MaxLength(100, ErrorMessage = "Please use an Address less than 100 letters.")]
+    public string Line1 { get; set; }
 
-        public string Line2 { get; set; }
+    [MaxLength(100)]
+    public string Line2 { get; set; }
 
-        public string City { get; set; }
+    [Required, MinLength(3, ErrorMessage = "Please use a City bigger than 3 letters."), MaxLength(50, ErrorMessage = "Please use a City less than 50 letters.")]
+    public string City { get; set; }
 
-        public string State { get; set; }
+    [Required, MinLength(2, ErrorMessage = "Please use a State bigger than 2 letters."), MaxLength(20, ErrorMessage = "Please use a State less than 20 letters.")]
+    public string State { get; set; }
 
-        public string PostalCode { get; set; }
-    }
+    [Required, RegularExpression(@"^([0-9]{5})$", ErrorMessage = "Please use a valid Postal Code with five numbers.")]
+    public string PostalCode { get; set; }
+}
 }
